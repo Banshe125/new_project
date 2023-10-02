32757abb88822470cb2b73309dbf14465e72cd6a
maksim <maksimov_maksime@mail.ru>
2023-10-02 23:00:36 +0300

    add new function_2 and add docstring

--- str_func.py
+++ str_func.py
@@ -1,7 +1,14 @@
-'''text upper'''
+''' text_up and'''
+
+text =input()
 def up_text():
-    text = input()
     text_up = text.upper()
     print(text_up)
 
 up_text()
+
+def up_first_latters():
+    text_up = text.title()
+    print(text_up)
+
+up_first_latters()
